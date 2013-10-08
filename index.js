44105bd93294f8c64fcd98531b61575fddb67c2a
Paul Sweeney <paul@8-bitdesign.com>
2013-10-08 16:11:48 -0700

    List stories, get entities and open entities

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 var path = require('path')
+  , exec = require('child_process').exec
   , config = require(path.join(process.env.HOME, '.targetprocess.json'))
   , tp = require('tp-api')({
       domain: config.domain,
       token: config.token
     })
 
-tp(process.argv.pop())
-  .take(5)
-  .where("EntityState.Name eq 'Open'")
-  .pluck('Name', 'NumericPriority')
-  .sortBy('NumericPriority')
-  .then(function(err, body) {
-    console.log(body)
+var entity = process.argv[2]
+  , amount = process.argv[3] || 100
+
+if (process.argv[2] === 'open') {
+  exec('open https://'+config.domain+'/entity/'+process.argv[3])
+} else if (process.argv[2]) {
+  tp(entity).take(amount).then(function(err, body) {
+    console.log(JSON.stringify(body, null, 2))
     if (err) console.error('err', err)
-  }
-)
+  })
+} else {
+  tp('Context').then(function(err, ctx) {
+    tp('Tasks').
+      where("(AssignedUser.Id eq "+ctx.LoggedUser.Id+") and (EntityState.Name ne 'Done')").
+      take(100).
+      pluck('Name', 'Description', 'UserStory[Name]', 'Team[Name]', 'Project[Name]').
+      sortBy('NumericPriority').
+      then(function(err, tasks) {
+        tasks.forEach(function(task) {
+          console.log('['+task.Id+'] '+task.Name);
+        })
+      })
+  })
+}
+
