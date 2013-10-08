1b4d7ee32c14da4097119bcb10ecab3ac64a6ffc
Paul Sweeney <paul@8-bitdesign.com>
2013-10-08 00:20:40 -0700

    Updated to reflect new api

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
-var request = require("request")
-  , fs = require('fs')
-  , path = require('path')
-  , Api = require('tp-api')
-
-var config = require(path.join(process.env.HOME, '.targetprocess.json'))
-  , tp = new Api({
+var path = require('path')
+  , config = require(path.join(process.env.HOME, '.targetprocess.json'))
+  , tp = require('tp-api')({
       domain: config.domain,
       token: config.token
     })
 
-function isntState(state) {
-  return function(item) {
-    return item.EntityState.Name !== state
+tp(process.argv.pop())
+  .take(5)
+  .where("EntityState.Name eq 'Open'")
+  .pluck('Name', 'NumericPriority')
+  .sortBy('NumericPriority')
+  .then(function(err, body) {
+    console.log(body)
+    if (err) console.error('err', err)
   }
-}
-
-function sortBy(prop) {
-  return function(a, b) {
-    if (a.NumericPriority > b.NumericPriority) {
-      return 1
-    } else if (a.NumericPriority < b.NumericPriority) {
-      return -1
-    } else {
-      return 0
-    }
-  }
-}
-
-var dingus = process.argv.pop()
-
-tp[dingus](function(err, body) {
-  console.log(dingus, body)
-  if (err) console.error('err', err)
-})
-
-tp.tasks(function(err, tasks) {
-})
+)
